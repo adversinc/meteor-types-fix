16912bad6868aecd8df297df5de552f848121d7f
Dino Fabrello <oleg@borodkin.ru>
2020-09-22 17:10:27 +0300

    Add ReactiveDictStrict

--- @types/meteor-fixes/reactive-dict.d.ts
+++ @types/meteor-fixes/reactive-dict.d.ts
@@ -7,3 +7,19 @@ declare interface ReactiveDict<U,T> {
 	get(key: U): T;
 	set(key: U, newValue: T): void;
 }
+
+/**
+ * Strict typed version of ReactiveDict.
+ *
+ * Required since TypeScript does not allow overriding declarations, and
+ * @types/Meteor adds its own relaxed ReactiveDict
+ */
+declare interface ReactiveDictStrict<U, T> extends ReactiveDict<U, T> {
+	get(key: U): T;
+	set(key: U, newValue: T): void;
+
+	setDefault(key: U, value?: T): void;
+	equals(key: U, value: T | undefined | null): boolean;
+	clear(): void;
+	destroy(): void;
+}
