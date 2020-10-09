e473045b12035b609336f5cda85c3d7bb972ac04
Dino Fabrello <oleg@borodkin.ru>
2020-10-09 18:30:02 +0300

    Add typed Template.instance() support

--- @types/meteor-fixes/meteor-misc.d.ts
+++ @types/meteor-fixes/meteor-misc.d.ts
@@ -1,7 +1,9 @@
 // This one is to fix errors in 'Template.instance().something'
-interface TemplateStatic {
-	instance(): any;
+interface TemplateStatic<T> {
+	instance(): T;
 }
+
+declare var Template: TemplateStatic<unknown>;
 
 declare module Meteor {
 	function callPromise<T>(name: string, ...args: any[]): Promise<T>;
@@ -12,7 +14,7 @@ declare module Blaze {
 	interface EventsMap {
 		// helper for event entries
 		// @ts-ignore Overwrites the @types/meteor
-		[key: string]: (event: MouseEvent, template: Template) => void;
+		[key: string]: (event: MouseEvent, template: Template) => void|boolean;
 	}
 
 	const _globalHelpers: {
