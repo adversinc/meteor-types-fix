a225867a1dda54ab3d034d37f10dde5bcd6a3fd3
Glaznah Gassner <dinofabrello@slbiz2life.com>
2022-04-17 17:12:09 +0400

    Up to recent @types/meteor

--- @types/meteor-fixes/meteor-misc.d.ts
+++ @types/meteor-fixes/meteor-misc.d.ts
@@ -1,23 +1,4 @@
-// This one is to fix errors in 'Template.instance().something'
-interface TemplateStatic<T> {
-	instance(): T;
-}
-
-declare var Template: TemplateStatic<unknown>;
-
 declare module Meteor {
 	function callPromise<T>(name: string, ...args: any[]): Promise<T>;
 	function _sleepForMs(ms: number): void;
-}
-
-declare module Blaze {
-	interface EventsMap {
-		// helper for event entries
-		// @ts-ignore Overwrites the @types/meteor
-		[key: string]: (event: MouseEvent, template: Template) => void|boolean;
-	}
-
-	const _globalHelpers: {
-		[name: string]: (...args: any[]) => any;
-	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 Additional fixes and hacks to avoid TypeScript 
 compilation warnings for Meteor.
+
+## Updating to v2
+
+Fixed types v2 have the most of Template extensions 
+removed. Current implementation of @types/meteor
+provides all the necessary typings.
+
+StrictDict<T> is now a type synonym for ReactiveDict<T>
+for a backward compatibility.
